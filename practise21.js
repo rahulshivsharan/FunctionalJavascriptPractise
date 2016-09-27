f5912a54dd861c1efc85a7f3e09a24a082777a82
Rahul Shivsharan <rahul.shivsharan@ril.com>
2016-09-27 17:34:36 +0530

    Added code for concatinating array 	1. added cat function which concats the array's to single array 	2. added construct function which takes primary element and an array, and outputs the array containing everythin 	3. added mapcat function which puts any value in between the array which is passed as second argument 	4. added butlast and interpose function which put elemets in array elements alternatively but not at last

--- practise21.js
+++ practise21.js
@@ -27,16 +27,59 @@ function cat(){
 }
 
 
-/// Just for examplanation
-console.log([].concat.apply([1,2,3],[[13,45],[15,67,89],67]));
-console.log([].concat.apply([1,2,3],[13,45,15,67,89,67]));
+/// Just for explanation
+// console.log([].concat.apply([1,2,3],[[13,45],[15,67,89],67]));
+// console.log([].concat.apply([1,2,3],[[13,45],[15],67,[89,67]]));
 
 
 
-//console.log(cat([1,2,3],[4,5,6],[7,8]));
+ //console.log(cat([1,2,3],[4,5,6],[7,8]));
 
 function construct(head,tail){
 	return cat([head],_.toArray(tail));
 }
 
 //console.log(construct(12,[24,36,48]));
+
+function mapcat(fn,col){
+	return cat.apply(null,_.map(col,fn));
+}
+
+/*
+console.log(mapcat(function(num){
+	return [num * 2];
+},[2,3,5]));
+*/
+
+
+/*
+console.log(mapcat(function(num){
+	return construct((num * 2),["--"]);
+},[2,3,5]));
+*/
+
+
+/*
+console.log((function(){
+	return _.toArray(arguments);
+})(1,2,3,4,5));
+*/
+
+function butLast(){
+	return _.toArray(arguments).slice(0,-1);
+}
+
+//console.log(butLast(1,2,3,4,5));
+
+
+
+function interpose(inter,coll){	
+	return butLast.apply(null,mapcat(function(e){
+		return construct(e,[inter]);
+	},coll));
+}
+
+console.log(interpose(",",[1,2,3]));
+
+
+
