4417ddb43d305f5629dbb03a60619cdca078e628
Rahul <rahul@duretechnologies.com>
2017-07-18 15:52:51 +0530

    Functional Programming demo 1
    	passing the business logic as functional paramter to enclosing
    	function which that call it internally

--- ch01.js
+++ ch01.js
@@ -16,3 +16,24 @@ underScore.times(4,function(){
 [12,34,56,78,90].forEach(function(val,index){
 	console.log(index," Number ",val);
 });
+
+function spat(fn){
+	return function(myArray){
+		return fn.apply(null,myArray);
+	}
+}
+
+var addAllNumbers = spat(function(){
+	var total = 0;	
+	for(var index = 0; index < arguments.length; index++){
+		total += arguments[index];
+	}
+
+	return total;
+});
+
+var array_numbers = [20,30,40];
+console.log(" Total of ",array_numbers," is ",addAllNumbers(array_numbers));
+
+array_numbers = [10,10,40,30];
+console.log(" Total of ",array_numbers," is ",addAllNumbers(array_numbers));
