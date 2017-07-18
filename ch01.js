320a77f4e23cb4a760acf7efb3ccecb26870ac61
Rahul <rahul@duretechnologies.com>
2017-07-18 15:26:22 +0530

    usage of noConflict
    	If I need to used give aliase to underscore api with other name,
    	I need to use noConflict

--- ch01.js
+++ ch01.js
@@ -3,3 +3,11 @@ var _ = require("underscore");
 _.times(4,function(){
 	console.log(" Hi There ");
 });
+
+
+var underScore = _.noConflict();
+
+underScore.times(4,function(){
+	var date = new Date();
+	console.log(" No Conflict ",date.getTime());
+});
